d540ba834395178fa1a6276efd7c92517cea48b7
Kornel Cholascinski <kornel_cholascinski@epam.com>
2025-04-16 15:59:46 +0200

    Multithreading task 4

--- 01_multithreading/MultiThreading.Task4.Threads.Join/Program.cs
+++ 01_multithreading/MultiThreading.Task4.Threads.Join/Program.cs
@@ -10,25 +10,54 @@
  */
 
 using System;
+using System.Threading;
 
 namespace MultiThreading.Task4.Threads.Join
 {
     class Program
     {
-        static void Main(string[] args)
+        private static readonly SemaphoreSlim semaphore = new SemaphoreSlim(1);
+
+        static void Main()
         {
-            Console.WriteLine("4.	Write a program which recursively creates 10 threads.");
-            Console.WriteLine("Each thread should be with the same body and receive a state with integer number, decrement it, print and pass as a state into the newly created thread.");
-            Console.WriteLine("Implement all of the following options:");
-            Console.WriteLine();
-            Console.WriteLine("- a) Use Thread class for this task and Join for waiting threads.");
-            Console.WriteLine("- b) ThreadPool class for this task and Semaphore for waiting threads.");
-
-            Console.WriteLine();
-
-            // feel free to add your code
+            CreateThreadsThroughJoin(10);
+            CreateThreadsThroughSemaphore(10);
 
             Console.ReadLine();
+        }
+
+        private static void CreateThreadsThroughSemaphore(int leftToCreate)
+        {
+            if (leftToCreate <= 0)
+                return;
+
+            ThreadPool.QueueUserWorkItem(state =>
+            {
+                semaphore.Wait();
+                LogLeft(leftToCreate);
+                CreateThreadsThroughSemaphore(leftToCreate - 1);
+                semaphore.Release();
+            });
+        }
+
+        private static void CreateThreadsThroughJoin(int leftToCreate)
+        {
+            if (leftToCreate <= 0)
+                return;
+
+            Thread thread = new Thread(() =>
+            {
+                LogLeft(leftToCreate);
+                CreateThreadsThroughJoin(leftToCreate - 1);
+            });
+
+            thread.Start();
+            thread.Join();
+        }
+
+        private static void LogLeft(int leftToCreate)
+        {
+            Console.WriteLine($"Created new thread, left to create: {leftToCreate}");
         }
     }
 }
