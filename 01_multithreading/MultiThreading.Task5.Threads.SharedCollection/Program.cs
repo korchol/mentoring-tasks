0ec3c4b84bd3a746ff5f69982b4e51b0a8482f52
Kornel Cholascinski <kornel_cholascinski@epam.com>
2025-04-16 16:40:41 +0200

    Multithreading task 5

--- 01_multithreading/MultiThreading.Task5.Threads.SharedCollection/Program.cs
+++ 01_multithreading/MultiThreading.Task5.Threads.SharedCollection/Program.cs
@@ -5,21 +5,52 @@
  * Use Thread, ThreadPool or Task classes for thread creation and any kind of synchronization constructions.
  */
 using System;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace MultiThreading.Task5.Threads.SharedCollection
 {
     class Program
     {
-        static void Main(string[] args)
+        const int ElementsNumber = 10;
+        static readonly AutoResetEvent AddEvent = new(false);
+        static readonly AutoResetEvent PrintEvent = new(false);
+
+        static void Main()
         {
-            Console.WriteLine("5. Write a program which creates two threads and a shared collection:");
-            Console.WriteLine("the first one should add 10 elements into the collection and the second should print all elements in the collection after each adding.");
-            Console.WriteLine("Use Thread, ThreadPool or Task classes for thread creation and any kind of synchronization constructions.");
-            Console.WriteLine();
+            var collection = new List<int>();
 
-            // feel free to add your code
+            var addingThread = new Thread(() => AddElements(collection));
 
-            Console.ReadLine();
+            var printingThread = new Thread(() => PrintElements(collection));
+
+            addingThread.Start();
+            printingThread.Start();
+
+            addingThread.Join();
+            printingThread.Join();
+
+            Console.WriteLine("Completed.");
+        }
+        private static void AddElements(List<int> collection)
+        {
+            for (int i = 0; i < ElementsNumber; i++)
+            {
+                collection.Add(i);
+                Console.WriteLine($"Added element: {i}");
+                PrintEvent.Set();
+                AddEvent.WaitOne();
+            }
+        }
+
+        private static void PrintElements(List<int> collection)
+        {
+            for (int i = 0; i < ElementsNumber; i++)
+            {
+                PrintEvent.WaitOne();
+                Console.WriteLine($"Collection state: {string.Join(", ", collection)}");
+                AddEvent.Set();
+            }
         }
     }
 }
