e484eeee8f87b7d2933ea1adb535394dc2bf5633
Kornel Cholascinski <kornel_cholascinski@epam.com>
2025-05-27 10:26:49 +0200

    Implement Task 1 and 2

--- 06_expressions/ExpressionTrees.Task1.ExpressionsTransformator/IncDecExpressionVisitor.cs
+++ 06_expressions/ExpressionTrees.Task1.ExpressionsTransformator/IncDecExpressionVisitor.cs
@@ -1,9 +1,49 @@
-﻿using System.Linq.Expressions;
+﻿using System.Collections.Generic;
+using System.Linq.Expressions;
 
 namespace ExpressionTrees.Task1.ExpressionsTransformer
 {
     public class IncDecExpressionVisitor : ExpressionVisitor
     {
-        // todo: feel free to add your code here
+        private readonly Dictionary<string, object> _parameterReplace;
+
+        public IncDecExpressionVisitor(Dictionary<string, object> parameterReplace)
+        {
+            _parameterReplace = parameterReplace;
+        }
+
+        protected override Expression VisitLambda<T>(Expression<T> node)
+        {
+            var newBody = Visit(node.Body);
+
+            return Expression.Lambda(newBody, new ParameterExpression[0]);
+        }
+
+        protected override Expression VisitBinary(BinaryExpression node)
+        {
+            if (node.Right is ConstantExpression constant && constant.Value is int value && value == 1)
+            {
+                if (node.NodeType == ExpressionType.Add)
+                {
+                    return Expression.Increment(Visit(node.Left));
+                }
+                if (node.NodeType == ExpressionType.Subtract)
+                {
+                    return Expression.Decrement(Visit(node.Left));
+                }
+            }
+
+            return base.VisitBinary(node);
+        }
+
+        protected override Expression VisitParameter(ParameterExpression node)
+        {
+            if (_parameterReplace.TryGetValue(node.Name, out var replacementValue))
+            {
+                return Expression.Constant(replacementValue, node.Type);
+            }
+
+            return base.VisitParameter(node);
+        }
     }
 }
--- 06_expressions/ExpressionTrees.Task2.ExpressionMapping/MappingGenerator.cs
+++ 06_expressions/ExpressionTrees.Task2.ExpressionMapping/MappingGenerator.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
 
 namespace ExpressionTrees.Task2.ExpressionMapping
 {
@@ -8,9 +11,33 @@ namespace ExpressionTrees.Task2.ExpressionMapping
         public Mapper<TSource, TDestination> Generate<TSource, TDestination>()
         {
             var sourceParam = Expression.Parameter(typeof(TSource));
+
+            var destination = Expression.New(typeof(TDestination));
+
+            List<MemberBinding> bindings = new List<MemberBinding>();
+
+            var sourceProperties = typeof(TSource).GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+            var destinationProperties = typeof(TDestination).GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+            foreach (var destProp in destinationProperties)
+            {
+                var sourceProp = sourceProperties.FirstOrDefault(sp => sp.Name == destProp.Name && sp.PropertyType == destProp.PropertyType);
+
+                if (sourceProp != null)
+                {
+                    var sourceValue = Expression.Property(sourceParam, sourceProp);
+
+                    var binding = Expression.Bind(destProp, sourceValue);
+
+                    bindings.Add(binding);
+                }
+            }
+
+            var body = Expression.MemberInit(destination, bindings);
+
             var mapFunction =
                 Expression.Lambda<Func<TSource, TDestination>>(
-                    Expression.New(typeof(TDestination)),
+                    body,
                     sourceParam
                 );
 
